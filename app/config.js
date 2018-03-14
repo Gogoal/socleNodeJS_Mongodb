d895cf23ce56506d8756dea612b9dcd3af9838fa
Mignot, Samuel <samuel.mignot@cgi.com>
2018-03-14 16:29:37 +0100

    Change link prod

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@ export const config = {
   dbDev: 'mongodb://localhost:27017/socleMongo',
   dbTest: 'mongodb://localhost:27017/testSocleMongo',
   apiLocal: 'http://localhost:3000',
-  apiProd: 'https://afternoon-dawn-41835.herokuapp.com/',
+  apiProd: 'https://afternoon-dawn-41835.herokuapp.com',
 };
 
 // Mail Wesport
